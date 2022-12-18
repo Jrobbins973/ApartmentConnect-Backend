8fdea5d930b048d5a3895f2e7bfd59b65fec26bb
Joe Robbins <joe.robbins.cpt@gmail.com>
2022-12-18 11:37:49 -0500

    added method to get full name for forum posts

--- app/models/forum_post.rb
+++ app/models/forum_post.rb
@@ -1,4 +1,8 @@
 class ForumPost < ApplicationRecord
     belongs_to :tenant 
     has_many :forum_post_replies
+
+    def full_tenant_name 
+        "#{tenant.first_name} #{tenant.last_name}"
+    end
 end
