7b05a5641e76dc9faa4daeb7cf788862ba36e61a
Joe Robbins <joe.robbins.cpt@gmail.com>
2022-12-30 17:30:07 -0500

    added create comment route

--- app/controllers/forum_post_replies_controller.rb
+++ app/controllers/forum_post_replies_controller.rb
@@ -1,2 +1,15 @@
 class ForumPostRepliesController < ApplicationController
+
+    # CREATE NEW
+
+    def create
+    reply = ForumPostReply.create!(reply_params)
+    render json: reply, status: :created
+    end
+
+    private
+
+    def reply_params
+        params.permit(:forum_post_id, :tenant_id, :text) 
+    end
 end
